1ea4adbd197aebc856c6550d68a384ef48f50eef
Si Yun (Steven Kim) <dev@donga.ac.kr>
2021-01-23 03:51:08 +0900

    * TItlePanel 작업중

---
+++ steven-2d-client/steven-2d-client/Assets/Scripts/Component_TitlePanel/TitlePanel.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+namespace steven
+{
+    public class TitlePanel : MonoBehaviour
+    {
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ steven-2d-client/steven-2d-client/Assets/Scripts/TitleScene/TitleScene.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening.Core;
+
+namespace steven
+{
+
+    public class TitleScene : MonoBehaviour
+    {
+        public Button StartButton;
+        public Button OptionButton;
+        public Button QuitButton;
+
+    }
+
+}
